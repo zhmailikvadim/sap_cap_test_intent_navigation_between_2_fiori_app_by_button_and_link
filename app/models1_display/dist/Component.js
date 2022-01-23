19ff12baefe62d509e3d097b28afd5dec1d56971
Vadzim Zhmailik <spongebobsp22@gmail.com>
2022-01-23 10:55:57 +0000

    3 app is working

---
+++ app/models1_display/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("models1display.Component",{metadata:{manifest:"json"}})});
--- app/services.cds
+++ app/services.cds
@@ -1,3 +1,4 @@
 
 using from './fiorikinds/annotations';
 using from './fiorimodels/annotations';
+using from './models1_display/annotations';
